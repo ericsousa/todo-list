1db635a0be032dd3089067e4be3b4a65c5421b37
Eric Sousa <ericsousa@gmail.com>
2019-04-10 14:14:41 -0300

    initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,25 @@
+// Check off Specific Todos By Clicking
+$('ul').on('click', 'li', function () {
+  $(this).toggleClass('completed')
+})
+
+// Click on X to delete Todo
+$('ul').on('click', 'span', function (e) {
+  $(this).parent().fadeOut(500, function () {
+    // this $(this) referes to the parent (li)
+    $(this).remove()
+  })
+  event.stopPropagation()
+})
+
+$('input[type="text"]').keypress(function (e) {
+  // check for enter key
+  if (e.which === 13) {
+    // grabbing new todo text from input
+    var todoText = $(this).val()
+    // clear input
+    $(this).val('')
+    // create a enw li and add to ul
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+  }
+})
