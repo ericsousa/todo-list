c077f55c1fdeb86f20dc4a461c0a679bcee53595
Eric Sousa <ericsousa@gmail.com>
2019-04-10 15:23:16 -0300

    add icons from fontawesome

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -20,6 +20,6 @@ $('input[type="text"]').keypress(function (e) {
     // clear input
     $(this).val('')
     // create a enw li and add to ul
-    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+    $('ul').append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoText + '</li>')
   }
 })
