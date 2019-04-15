bb602084c77040a014bca5c9b26ad00f74b8c2f0
Eric Sousa <ericsousa@gmail.com>
2019-04-15 14:32:27 -0300

    refator to pure js, no jquery

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,29 +1,65 @@
-// Check off Specific Todos By Clicking
-$('ul').on('click', 'li', function () {
-  $(this).toggleClass('completed')
-})
+let ul = document.querySelector('ul')
+let input = document.querySelector('input[type="text"]')
+let plusBtn = document.querySelector('.fa-plus')
 
-// Click on X to delete Todo
-$('ul').on('click', 'span', function (e) {
-  $(this).parent().fadeOut(500, function () {
-    // this $(this) referes to the parent (li)
-    $(this).remove()
-  })
-  event.stopPropagation()
-})
+ul.addEventListener('click', function (e) {
+  // check off todo
+  if (e.target.nodeName === 'LI') {
+    e.target.classList.toggle('completed')
+  }
 
-$('input[type="text"]').keypress(function (e) {
-  // check for enter key
-  if (e.which === 13) {
-    // grabbing new todo text from input
-    var todoText = $(this).val()
-    // clear input
-    $(this).val('')
-    // create a enw li and add to ul
-    $('ul').append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoText + '</li>')
+  // delete todo
+  if (e.target.matches('span, .fa-trash-alt')) {
+    let li = e.target.closest('li') 
+
+     //fadeOut using css transition
+    li.classList.add('fadeOut')
+    setTimeout(function () {
+      li.remove()
+    }, 500)
+    e.stopPropagation()
   }
 })
 
-$('.fa-plus').click(function () {
-  $('input[type="text"]').fadeToggle();
+// add todo after pressing enter
+input.addEventListener('keypress', function (e) {
+  if (e.which === 13) {
+    let todoText = input.value
+    input.value = ''
+    let li = document.createElement('li')
+    li.innerHTML = `<span><i class="fas fa-trash-alt"></i></span>${todoText}`
+    ul.appendChild(li)
+  }
 })
+
+// toggel input box
+plusBtn.addEventListener('click', function () {
+  fadeToggle(input, 500)
+})
+
+
+function fadeToggle(elem, speed, callback) {
+  // check opacity, set fade function
+  if (!elem.style.opacity || Number(elem.style.opacity) === 1) {
+    elem.style.opacity = 1
+    fadeOut = true
+  } else {
+    fadeOut = false
+  }
+  // fade out, fade in animation
+  let interval = setInterval(function () {
+    if (fadeOut) {
+      elem.style.opacity -= 0.02
+      if (elem.style.opacity <= 0) {
+        clearInterval(interval)
+        elem.style.display = 'none'
+      }
+    } else {
+      elem.style.display = 'block'
+      elem.style.opacity = Number(elem.style.opacity) + 0.02
+      if (elem.style.opacity >= 1) {
+        clearInterval(interval)
+      }
+    }
+  }, speed/50) 
+}
